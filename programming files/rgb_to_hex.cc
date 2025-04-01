f0eb71be299a26ee075cf233c0250210726e5d38
TedPittman <113031477+TedPittman@users.noreply.github.com>
2025-04-01 10:34:26 -0400

    Update rgb_to_hex.cc

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
-
+// bugs commited: TedPittman
 std::string rgb_to_hex(int r, int g, int b)
 {
     r = std::max(0, std::min(255, r));
     g = std::max(0, std::min(255, g));
     b = std::max(0, std::min(255, b));
 
-    std::stringstream ss;
-    ss << std::uppercase << std::hex << std::setfill('0')
+    std::stringstream ss
+    ss << std::uppercase << std::hex << std::setfill('0)
        << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
 
     return ss.str();
