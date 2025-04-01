754a823292f0f08a2928bcc8c25ee22e417c1437
TedPittman <113031477+TedPittman@users.noreply.github.com>
2025-04-01 11:09:20 -0400

    Update even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -7,7 +7,7 @@ int main() {
     ofstream file;
     file.open("numbers.html");
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
-    file << "<tbl>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
+    file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
     for (int i = 1; i <= 50; i++) {
         if (i % 2 == 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
@@ -16,10 +16,12 @@ int main() {
             file << "<tr><td></td><td>" << i << "</td></tr>\n";
         }
     }
-    file << "</tbl>\n</body>\n</html>";
+    file << "</table>\n</body>\n</html>";
     file.close();
     ifstream input("numbers.html");
     cout << input.rdbuf();
     input.close();
     return 0;
 }
+
+
