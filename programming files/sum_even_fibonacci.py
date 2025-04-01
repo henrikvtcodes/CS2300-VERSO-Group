1f8d1064dd0c50a30d139d3b685e498d3368a9b8
charlesw5099 <charlesw5099@gmail.com>
2025-04-01 11:02:33 -0400

    Fixed sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,13 @@
 # this program calculates the sum of all even fibonacci numbers less than 4 million
 a, b = 0, 1
 sum_even = 0
-while b <= 4000000:
-    if b % 3 == 0:
+# fixed from b <= 4000000
+while b < 4000000:
+    # fixed b % 2 for correct fibonacci sequence from b % 3
+    if b % 2 == 0:
         sum_even += b
-    a, b = a, a+b
+        # fixed from a, b = a, a+b
+    a, b = b, a+b
 print(sum_even)
 
 # answer should be 4613732
