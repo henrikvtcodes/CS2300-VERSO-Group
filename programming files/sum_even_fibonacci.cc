3e412627185594aa72e95eed184eb9fd45ffbf36
charlesw5099 <charlesw5099@gmail.com>
2025-04-01 11:03:43 -0400

    Fixed sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -9,12 +9,14 @@ int main() {
   int a = 0, b = 1;
   int sum_even = 0;
   while (b < 4000000) {
-    if (b % 3 == 0) {
+    // fixed from b % 3
+    if (b % 2 == 0) {
       sum_even += b;
     }
     int temp = b;
     b = a + b;
-    a = temp + 1;
+    // fixed from temp + 1
+    a = temp;
   }
   std::cout << sum_even << std::endl;
 
