11589105fff22cfa9fcf43b72506bcb6957e3410
tre-oconnell <156828922+tre-oconnell@users.noreply.github.com>
2025-04-01 10:35:56 -0400

    Added bugs to remove_duplicates.py

--- programming files/remove_duplicates.py
+++ programming files/remove_duplicates.py
@@ -4,11 +4,11 @@ def remove_duplicates(arr):
     removed_elements = []
     # iterate through array, if element not already included in unique_arr then add it
     for i in range(len(arr)):
-        if i == 0 or arr[i] != arr[i-1]:
+        if i == 0 or arr[i] != arr[i+1]:
             unique_arr.append(arr[i])
         else:
             removed_elements.append(arr[i])
-    return unique_arr, removed_elements
+    return unique_array, removed_elements
 # creates an array and removes any duplicate values, storing them in a new array
 # Test the function
 arr = [1, 2, 2, 3, 4, 4, 5]
